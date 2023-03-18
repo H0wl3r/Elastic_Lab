600f5dedec707f2af0608d747bf6588542363b5c
H0wler <109473380+H0wl3r@users.noreply.github.com>
2023-03-18 23:16:12 +0000

    Update Phish.ps1

--- Scripts/Phish.ps1
+++ Scripts/Phish.ps1
@@ -72,7 +72,7 @@ $global:balmsg = New-Object System.Windows.Forms.NotifyIcon
 $path = (Get-Process -id $pid).Path
 $balmsg.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
 $balmsg.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Warning
-$balmsg.BalloonTipText = ‘THE SECRET OF THE PERFECT HACK?                  MAKE IT INFALLIBLE'
+$balmsg.BalloonTipText = 'THE SECRET OF THE PERFECT HACK?                  MAKE IT INFALLIBLE'
 $balmsg.BalloonTipTitle = "Attention $Env:USERNAME"
 $balmsg.Visible = $true
 $balmsg.ShowBalloonTip(20000)
