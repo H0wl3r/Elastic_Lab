c25f8db502ff1ef8e91fdc6e4b8f2d6b864e69e5
H0wler <109473380+H0wl3r@users.noreply.github.com>
2023-03-18 11:56:17 +0000

    Update Ransom.ps1

--- Scripts/Ransom.ps1
+++ Scripts/Ransom.ps1
@@ -1,8 +1,8 @@
-Write-Host "Inputting Module"
+Write-Host "[+] Set up"
 Import-Module "C:\AtomicRedTeam\invoke-atomicredteam\Invoke-AtomicRedTeam.psd1" -Force
 Start-Sleep -S 15
 
-Write-Host "T1091"
+Write-Host "[+] T1091"
 Invoke-AtomicTest T1091
 Start-Sleep -S 15
 
