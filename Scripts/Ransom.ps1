d689cb1f9f009105157a5b935bd36dac64009ef5
H0wler <109473380+H0wl3r@users.noreply.github.com>
2023-03-20 13:27:06 +0000

    Update Ransom.ps1

--- Scripts/Ransom.ps1
+++ Scripts/Ransom.ps1
@@ -723,7 +723,7 @@ $global:balmsg = New-Object System.Windows.Forms.NotifyIcon
 $path = (Get-Process -id $pid).Path
 $balmsg.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
 $balmsg.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Warning
-$balmsg.BalloonTipText = ‘CONTROL CAN SOMTIMES BE AN ILLUSION. BUT SOMETIMES YOU NEED ILLUSION TO GAIN CONTROL.'
+$balmsg.BalloonTipText = "CONTROL CAN SOMTIMES BE AN ILLUSION. BUT SOMETIMES YOU NEED ILLUSION TO GAIN CONTROL.";
 $balmsg.BalloonTipTitle = "Attention $Env:USERNAME"
 $balmsg.Visible = $true
 $balmsg.ShowBalloonTip(20000)
